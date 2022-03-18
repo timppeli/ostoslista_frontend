0d9206a94b18de082900db734683a63b250fc43c
timppeli <91657796+timppeli@users.noreply.github.com>
2022-03-18 12:16:53 +0200

    Käyttöliittymä kovakoodattu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  
   return (
-    <div>
-     ...
+    <div className="container">
+      <h3>Shopping List</h3>
+      <form>
+        <label>New item: </label>
+        <input type="text" name="description" placeholder="Type description" />
+        <input type="text" name="amount" placeholder="Type amount" />
+        <input type="submit" value="Add" />
+      </form>
+      <ul className="items-container">
+        <li>
+          <div>Milk</div>
+          <div>2</div>
+          <div><a href="#">Delete</a></div>
+        </li>
+        <li>
+          <div>Bread</div>
+          <div>2</div>
+          <div><a href="#">Delete</a></div>
+        </li>
+      </ul>
     </div>
   );
 }
